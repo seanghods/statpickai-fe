c35a0c7f1b603debfc24d29fec27f737ccc2bf4a
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-12-20 00:52:24 -0800

    fix mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import useResponse from '../context/useResponse';
 import { API_ROUTES } from '../utils/constants';
 import MenuButtonX from './sub-components/MenuButton';
-import { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
 
 export default function Header() {
@@ -24,7 +23,6 @@ export default function Header() {
       console.log(data.message);
     }
   }
-  const [state, setState] = useState(false);
   const navUnlogged = [
     { title: 'Games', path: '/games' },
     { title: 'Pricing', path: '/pricing', class: 'hidden md:block' },
@@ -41,98 +39,17 @@ export default function Header() {
     },
   ];
 
-  useEffect(() => {
-    document.onclick = e => {
-      const target = e.target;
-      if (!target.closest('.menu-btn')) setState(false);
-    };
-  }, []);
-
-  const Brand = () => (
-    <div className="flex items-center justify-between py-5 md:block">
-      {/* <NavLink
-        to={user.username ? '/home' : '/'}
-        className="hover:scale-105 transform transition duration-250 h-full font-saira_bold text-white text-sm md:text-lg"
-      >
-        STAT <span className="text-[#4DE234]">PICK</span> AI
-      </NavLink> */}
-      <NavLink to={user.username ? '/home' : '/'}>
-        <img
-          src={logo}
-          alt="Stat Pick AI Logo"
-          className="h-[50px] w-[130px]"
-        />
-      </NavLink>
-      <div className="md:hidden">
-        <button
-          className="menu-btn text-gray-400 hover:text-gray-300"
-          onClick={() => setState(!state)}
-        >
-          {state ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          )}
-        </button>
-      </div>
-    </div>
-  );
   return (
     <header>
-      {/* <div className={`md:hidden ${state ? 'mx-2 pb-5' : 'hidden'}`}>
+      <div className="md:gap-x-12 items-center max-w-screen-xl mx-auto px-4 flex md:px-8 py-5">
         <NavLink to={user.username ? '/home' : '/'}>
           <img
             src={logo}
             alt="Stat Pick AI Logo"
-            className="h-[50px] w-[130px]"
+            className="h-[50px] md:w-[130px]"
           />
         </NavLink>
-      </div> */}
-      {/* <nav
-        className={`pb-5 md:text-sm ${
-          state
-            ? 'absolute z-20 top-0 inset-x-0 bg-gray-800 rounded-xl mx-2 mt-2 md:mx-0 md:mt-0 md:relative md:bg-transparent'
-            : ''
-        }`}
-      > */}
-      <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 flex md:px-8 py-5">
-        <NavLink to={user.username ? '/home' : '/'}>
-          <img
-            src={logo}
-            alt="Stat Pick AI Logo"
-            className="h-[45px] md:h-[50px] w-[130px]"
-          />
-        </NavLink>
-        {/* <div
-            className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
-              state ? 'block' : 'hidden'
-            } `}
-          > */}
-        <ul className="flex-1 gap-2 md:gap-1 justify-end items-center flex md:space-x-6 md:space-y-0">
+        <ul className="flex-1 gap-1 justify-end items-center flex md:space-x-6 md:space-y-0">
           {user.username ? (
             <>
               <div
@@ -159,7 +76,7 @@ export default function Header() {
                 return (
                   <li
                     key={idx}
-                    className="text-sm md:text-sm text-gray-300 hover:text-gray-400"
+                    className="text-sm md:text-base text-gray-300 hover:text-gray-400"
                   >
                     <NavLink to={item.path} className={item.class}>
                       {item.title}
@@ -181,7 +98,10 @@ export default function Header() {
             <>
               {navUnlogged.map((item, idx) => {
                 return (
-                  <li key={idx} className="text-gray-300 hover:text-gray-400">
+                  <li
+                    key={idx}
+                    className="text-sm md:text-base text-gray-300 hover:text-gray-400"
+                  >
                     <NavLink to={item.path} className={item.class}>
                       {item.title}
                     </NavLink>
