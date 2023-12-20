b41f93b45581656b7e4a86189c32e6ff9be2b771
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-12-20 05:44:01 -0800

    fix error

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as Header } from './Header.jsx';
 export { default as Footer } from './Footer.jsx';
 export { default as GameInfo } from './GameInfo.jsx';
-export { default as LogInModal } from './LogInModal.jsx';
 export { default as FullLoadingPage } from './FullLoadingPage.jsx';
 export { default as PaymentForm } from './PaymentForm.jsx';
 export { default as AllResponses } from './AllResponses.jsx';
